c5a912cbf2859b95ed356e5f191e8a7dae29ef2e
Miles Johann Bailey-Braendgaard <milesjohannbailey-braendgaard@miless-air.home>
2022-07-05 14:22:54 +0100

    ex 1-9 finished

--- index.js
+++ index.js
@@ -12,37 +12,54 @@ ASSIGNMENT RULES
  Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let firstname = "miles";
+// ^ A string represents text based data, i.e. words, letters, sentences, etc...
+
+let age = "22";
+// ^ A number/integer represents a numerical value, similar data types include BigInt (for larger numbers), double , single
+
+let bool = true;
+// ^ A boolean represents a true/false value
+
+let noval;
+// ^ Undefined variables haven't yet been given a data type or data to hold
+
+let nothing = null;
+// ^ A 'null' variable simply has been assigned as 'null', the intended absence of a value
+
+
 
 /* EXERCISE 2
  Try to describe what a variable is, in your own words.
 */
 
-/* WRITE YOUR ANSWER HERE */
+// A variable is a container for different data types that in some cases can be changed and in others are unchangable and are meant to be used as constants or in other operations and calculations
 
 /* EXERCISE 3
  Write the code to execute an addition (a sum) of the numbers 12 and 20.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let sum = 12 + 20;
 
 /* EXERCISE 4
  Create a variable named x and assign to it the number 12.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let y = 12;
+// because 'x' is used later on
 
 /* EXERCISE 5
  Create a variable called name and assign to it the string "John Doe".
 */
 
-/* WRITE YOUR ANSWER HERE */
+let name = "John Doe";
 
 /* EXERCISE 6
  Execute a subtraction between the number 12 and the variable x you declared before (which is storing the value 12).
 */
 
-/* WRITE YOUR ANSWER HERE */
+y -= 12;
+// because 'x' is used later on
 
 /* EXERCISE 7
  Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
@@ -50,18 +67,50 @@ ASSIGNMENT RULES
  Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
 */
 
-/* WRITE YOUR ANSWER HERE */
+let name1 = "john";
+let name2 = "John";
+
+let difcheck = (name1 === name2);
+console.log("============Excercise 7============")
+console.log("")
+console.log("Are name1 and name2 equal?");
+console.log(difcheck.toString());
+console.log("")
+name2 = "john";
+difcheck = (name1 === name2);
+console.log("Are name1 and name2 equal now?");
+console.log(difcheck.toString());
 
 /* EXERCISE 8
  Create a variable named x, and assign to it a numeric value less than 10.
  Write the code to print the literal value of x (ex.: if x is 1, it should print "one", if x is 5 it should print "five" etc.).
 */
 
-/* WRITE YOUR ANSWER HERE */
+let x = Math.floor(Math.random() * 10);
+let numbers = Array("one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+console.log("");
+console.log("============Excercise 8============");
+console.log("");
+console.log("Your random number is " + numbers[x] + ".");
+
 
 /* EXERCISE 9
  [Extra] Assign a value to a variable using a ternary operator (this topic has not been covered during lesson yet,
  try to search it by yourself, tomorrow morning we'll discuss it together!)
 */
 
-/* WRITE YOUR ANSWER HERE */
+let cat = Boolean(false);
+function getCat(cat) {
+    return (cat ? "YES" : "NO")
+}
+let iscat = getCat(cat);
+console.log("")
+console.log("============Excercice 9============")
+console.log("")
+console.log("Is that a cat?");
+console.log(iscat);
+console.log("")
+cat = true;
+iscat = getCat(cat);
+console.log("Is is a cat now?");
+console.log(iscat);
